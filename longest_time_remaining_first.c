db6cbc7e19d907f1c26102f6ba3302058a775952
Ayush Jain <ayushj250@gmail.com>
2018-03-27 23:49:29 +0530

    Longest remaining time first-initialization
    I have created a structure for student arriving in mess and taken variables like id,t_time,c_time,a_time,s_time for id,food taking time,completion time,arrival time,starting time respectively and intiliased them accordingly

---
+++ longest_time_remaining_first.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+struct student
+{
+	int id;
+	int t_time;
+	int c_time;
+	int a_time;
+	int s_time;
+}s1,s2,s3;
+int tat=0,wt=0,timer=0;
+int main()
+{
+	
+	s1.id=2132;
+	s1.t_time=2;
+	s1.a_time=0;
+	s2.id=2102;
+	s2.t_time=4;
+	s2.a_time=0;
+	s3.id=2453;
+	s3.t_time=8;
+	s3.a_time=0;
+	
+}
